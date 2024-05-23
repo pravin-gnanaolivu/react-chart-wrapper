d06b253649e8cfa8b71f2497780931ebcfea1a75
pravin-gnanaolivu <pravinkumar@Pravins-MacBook-Air.local>
2024-05-23 12:08:23 +0530

    Handle regisrty in laypout effect

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,30 +1,24 @@
-import React, { useRef, useEffect } from "react";
-import { Chart as ChartJS, registerables } from "chart.js";
-import { Chart } from "react-chartjs-2";
-import { ChartTypeRegistry } from "chart.js";
-
-ChartJS.register(...registerables);
+import React from "react";
+import { Chart, registerables } from "chart.js";
+import { Chart as ChartJs } from "react-chartjs-2";
+import { type ChartTypeRegistry, ChartData, ChartOptions } from "chart.js";
 
 export interface ChartProps {
-  type: keyof ChartTypeRegistry;
-  data: any;
-  options: any;
+  type: keyof ChartTypeRegistry & string;
+  data: ChartData;
+  options: ChartOptions;
 }
 
 const ReactChart = ({ type, data, options }: ChartProps) => {
-  const chartRef = useRef<ChartJS | null>(null);
+  React.useLayoutEffect(() => {
+    Chart.register(...registerables);
 
-  useEffect(() => {
     return () => {
-      // Cleanup chart instance on unmount
-      if (chartRef.current) {
-        chartRef.current.destroy();
-        chartRef.current = null;
-      }
+      Chart.unregister(...registerables);
     };
   }, []);
 
-  return <Chart ref={chartRef} type={type} data={data} options={options} />;
+  return <ChartJs type={type} data={data} options={options} />;
 };
 
 export default ReactChart;
---
+++ src/components/Chart/chart-setup.ts
@@ -0,0 +1,26 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
+  RadialLinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
+  RadialLinearScale
+);
